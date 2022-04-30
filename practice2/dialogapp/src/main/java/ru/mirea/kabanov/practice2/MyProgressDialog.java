6cdda3850e5c35238b67c0a65c44149812d193c1
ZahaUdzumaha <zakhar_kabanov@mail.ru>
2022-04-30 17:42:37 +0300

    commit

---
+++ practice2/dialogapp/src/main/java/ru/mirea/kabanov/practice2/MyProgressDialog.java
@@ -0,0 +1,11 @@
+package ru.mirea.kabanov.practice2;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+
+public class MyProgressDialog extends ProgressDialog {
+
+    public MyProgressDialog(Context context) {
+        super(context);
+    }
+}
--- practice2/lifecycleactivity/src/main/java/ru/mirea/kabanov/lifecycleactivity/MainActivity.java
+++ practice2/lifecycleactivity/src/main/java/ru/mirea/kabanov/lifecycleactivity/MainActivity.java
@@ -1,93 +1,68 @@
 package ru.mirea.kabanov.lifecycleactivity;
 
-import static android.content.ContentValues.TAG;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
+
+    public static String TAG = MainActivity.class.getSimpleName();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i(TAG, "onCreate()");
-    }
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState)
-    {
-        super.onCreate(savedInstanceState, persistentState);
-        Log.i(TAG, "onCreate()");
+
+        Log.i(TAG,"onCreate()");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(TAG, "onStart()");
+        Log.i(TAG,"onStart()");
     }
 
     @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        Log.i(TAG, "onRestoreInstanceState()");
+        Log.i(TAG,"onRestoreInstanceState()");
     }
 
     @Override
-    public void onPostCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onPostCreate(savedInstanceState, persistentState);
-        Log.i(TAG, "onPostCreate()");
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG,"onRestart()");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(TAG, "onResume()");
-    }
-
-    @Override
-    protected void onPostResume() {
-        super.onPostResume();
-        Log.i(TAG, "onPostResume()");
-    }
-
-    @Override
-    public void onAttachedToWindow() {
-        super.onAttachedToWindow();
-        Log.i(TAG, "onAttachedToWindow()");
+        Log.i(TAG,"onResume()");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i(TAG, "onPause()");
+        Log.i(TAG,"onPause()");
     }
 
     @Override
-    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
-        super.onSaveInstanceState(outState, outPersistentState);
-        Log.i(TAG, "onSaveInstanceState()");
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG,"onSaveInstanceState()");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i(TAG, "onStop()");
+        Log.i(TAG,"onStop()");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i(TAG, "onDestroy()");
-    }
-
-    @Override
-    public void onDetachedFromWindow() {
-        super.onDetachedFromWindow();
-        Log.i(TAG, "onDetachedFromWindow()");
+        Log.i(TAG,"onDestroy()");
     }
 }
