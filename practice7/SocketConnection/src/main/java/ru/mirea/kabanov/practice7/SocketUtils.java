a530ebca15955945178ec65be4039ca1bf8daa76
ZahaUdzumaha <zakhar_kabanov@mail.ru>
2022-05-03 15:16:39 +0300

    practice 7-8

---
+++ practice7/SocketConnection/src/main/java/ru/mirea/kabanov/practice7/SocketUtils.java
@@ -0,0 +1,13 @@
+package ru.mirea.kabanov.practice7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.net.Socket;
+
+public class SocketUtils {
+    public static BufferedReader getReader(Socket s) throws IOException {
+        return (new BufferedReader(new InputStreamReader(s.getInputStream())));
+    }
+}
